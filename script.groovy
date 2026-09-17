38b3868b874d5ea0a3fcbe43eb1710e5211e0201
Yhlas Nurmuhammedov <154041709+Nurmuhammedov12@users.noreply.github.com>
2026-09-17 16:00:52 +0200

    Fix typo in docker login command

--- script.groovy
+++ script.groovy
@@ -2,7 +2,7 @@ def buildImage(){
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
         sh 'docker build -t nurmuhammedowyhlas/todo-app:jma-1.0 ./backend'
-        sh 'echo $PASS | docker login -u $USER --pasword-stdin'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'docker push nurmuhammedowyhlas/todo-app:jma-1.0'
 
    }             
